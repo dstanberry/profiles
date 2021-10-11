ba5a8d7644e0ab51f978d04f0c1067b9c11a8d1e
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-10-11 11:45:32 -0500

    powershell: update prompt and wsl interop

--- powershell/Powershell_WSL.ps1
+++ powershell/Powershell_WSL.ps1
@@ -9,22 +9,21 @@ Remove-Item Alias:\where -Force -ErrorAction SilentlyContinue
 ################################################################################
 # WSL Interpop
 ################################################################################
-Import-WslCommand "awk", "bat", "colordiff", "cp", "curl", "cut", "diff", "diff-highlight", "emacs", "git", "grep", "head", "less", "lnav", "ls", "man", "mv", "nano", "rm", "sed", "seq", "sh", "tail", "tmux", "tree", "vim", "wget", "which", "xargs"
+Import-WslCommand "awk", "bat", "colordiff", "cp", "curl", "cut", "diff", "diff-highlight", "emacs", "exa", "git", "grep", "head", "less", "lnav", "ls", "man", "mv", "nano", "nvim", "rm", "sed", "seq", "sh", "tail", "tmux", "tree", "which", "xargs"
 
 $WslDefaultParameterValues = @{ }
-$WslDefaultParameterValues["ls"] = "--ignore='ntuser\.*' --ignore='NTUSER\.*' --almost-all --color=auto --group-directories-first"
+$WslDefaultParameterValues["exa"] = "--ignore-glob='ntuser\.*' --ignore-glob='NTUSER\.*' --all --group-directories-first"
 $WslDefaultParameterValues["grep"] = "--color=auto"
+$WslDefaultParameterValues["ls"] = "--ignore='ntuser\.*' --ignore='NTUSER\.*' --almost-all --color=auto --group-directories-first"
 
 $WslEnvironmentVariables = @{ }
-$WslEnvironmentVariables["EDITOR"] = "/usr/bin/vim"
+$WslEnvironmentVariables["EDITOR"] = "/usr/bin/nvim"
 $WslEnvironmentVariables["HISTCONTROL"] = "erasedups:ignoreboth"
 $WslEnvironmentVariables["LANG"] = "en_US.utf8"
 $WslEnvironmentVariables["LESS"] = "-iFMRX"
 $WslEnvironmentVariables["LESSCOLOR"] = "always"
 $WslEnvironmentVariables["LESSCOLORIZER"] = "/usr/bin/src-hilite-lesspipe.sh"
 $WslEnvironmentVariables["LS_COLORS"] = "no=00:rs=0:fi=00:di=01;34:ln=36:mh=04;36:pi=04;01;36:so=04;33:do=04;01;36:bd=01;33:cd=33:or=31:mi=01;37;41:ex=01;36:su=01;04;37:sg=01;04;37:ca=01;37:tw=01;37;44:ow=01;04;34:st=04;37;44:*.7z=01;32:*.ace=01;32:*.alz=01;32:*.arc=01;32:*.arj=01;32:*.bz=01;32:*.bz2=01;32:*.cab=01;32:*.cpio=01;32:*.deb=01;32:*.dz=01;32:*.ear=01;32:*.gz=01;32:*.jar=01;32:*.lha=01;32:*.lrz=01;32:*.lz=01;32:*.lz4=01;32:*.lzh=01;32:*.lzma=01;32:*.lzo=01;32:*.rar=01;32:*.rpm=01;32:*.rz=01;32:*.sar=01;32:*.t7z=01;32:*.tar=01;32:*.taz=01;32:*.tbz=01;32:*.tbz2=01;32:*.tgz=01;32:*.tlz=01;32:*.txz=01;32:*.tz=01;32:*.tzo=01;32:*.tzst=01;32:*.war=01;32:*.xz=01;32:*.z=01;32:*.Z=01;32:*.zip=01;32:*.zoo=01;32:*.zst=01;32:*.aac=32:*.au=32:*.flac=32:*.m4a=32:*.mid=32:*.midi=32:*.mka=32:*.mp3=32:*.mpa=32:*.mpeg=32:*.mpg=32:*.ogg=32:*.opus=32:*.ra=32:*.wav=32:*.3des=01;35:*.aes=01;35:*.gpg=01;35:*.pgp=01;35:*.doc=32:*.docx=32:*.dot=32:*.odg=32:*.odp=32:*.ods=32:*.odt=32:*.otg=32:*.otp=32:*.ots=32:*.ott=32:*.pdf=32:*.ppt=32:*.pptx=32:*.xls=32:*.xlsx=32:*.app=01;36:*.bat=01;36:*.btm=01;36:*.cmd=01;36:*.com=01;36:*.exe=01;36:*.reg=01;36:*~=02;37:*.bak=02;37:*.BAK=02;37:*.log=02;37:*.log=02;37:*.old=02;37:*.OLD=02;37:*.orig=02;37:*.ORIG=02;37:*.swo=02;37:*.swp=02;37:*.bmp=32:*.cgm=32:*.dl=32:*.dvi=32:*.emf=32:*.eps=32:*.gif=32:*.jpeg=32:*.jpg=32:*.JPG=32:*.mng=32:*.pbm=32:*.pcx=32:*.pgm=32:*.png=32:*.PNG=32:*.ppm=32:*.pps=32:*.ppsx=32:*.ps=32:*.svg=32:*.svgz=32:*.tga=32:*.tif=32:*.tiff=32:*.xbm=32:*.xcf=32:*.xpm=32:*.xwd=32:*.xwd=32:*.yuv=32:*.anx=32:*.asf=32:*.avi=32:*.axv=32:*.flc=32:*.fli=32:*.flv=32:*.gl=32:*.m2v=32:*.m4v=32:*.mkv=32:*.mov=32:*.MOV=32:*.mp4=32:*.mpeg=32:*.mpg=32:*.nuv=32:*.ogm=32:*.ogv=32:*.ogx=32:*.qt=32:*.rm=32:*.rmvb=32:*.swf=32:*.vob=32:*.webm=32:*.wmv=32:"
-$WslEnvironmentVariables["MYVIMRC"] = "~/.config/vim/vimrc"
-$WslEnvironmentVariables["VIMINIT"] = ":set runtimepath+=~/.config/vim,~/.config/vim/after|:set viminfo+=:20,%,n~/.config/vim/viminfo|:source ~/.config/vim/vimrc|:set runtimepath+=~/.config/vim,~/.config/vim/after"
 $WslEnvironmentVariables["MANPAGER"] = "manpager"
 $WslEnvironmentVariables["PAGER"] = "/usr/bin/less"
 $WslEnvironmentVariables["PWD"] = "/mnt/d"
@@ -153,7 +152,7 @@ function Test-Administrator {
 # Define prompt
 ################################################################################
 function prompt {
-	$realLASTEXITCODE = $LASTEXITCODE
+	$realLASTEXITCODE = $?
 
 	$history = Get-History -Count 1
 
@@ -184,15 +183,15 @@ function prompt {
 		}
 	}
 
-	$global:LASTEXITCODE = $realLASTEXITCODE
-
-	if ($realLASTEXITCODE -eq 0) {
+	if ($realLASTEXITCODE) {
 		Write-Host " $(U 0x276F)" -NoNewline -ForegroundColor Green -BackgroundColor Black
 	}
 	else {
 		Write-Host " $(U 0x276F)" -NoNewline -ForegroundColor Red -BackgroundColor Black
 	}
 	
+	Remove-Variable realLASTEXITCODE
+
 	Write-Host "" -NoNewline -ForegroundColor White
 	return " "
 }
