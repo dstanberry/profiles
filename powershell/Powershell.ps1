40c57a3eb0a714ba7d3f7245809264216ccfc1cf
Demaro Stanberry <demaro.stanberry@gmail.com>
2022-07-13 16:15:55 -0500

    add right justified content to prompt

--- powershell/Powershell.ps1
+++ powershell/Powershell.ps1
@@ -3,30 +3,111 @@ using namespace System.Management.Automation.Language
 
 $basedir = (Resolve-Path $env:USERPROFILE).ToString() + "\"
 
-if ((Get-Volume).DriveLetter -contains "D") {
+if ((Get-Volume).DriveLetter -contains "D")
+{
 	$basedir = (Resolve-Path "D:").ToString()
 }
 
-function U {
+function U
+{
 	param([int] $Code)
-	if ((0 -le $Code) -and ($Code -le 0xFFFF)) {
+	if ((0 -le $Code) -and ($Code -le 0xFFFF))
+	{
 		return [char] $Code
 	}
 
-	if ((0x10000 -le $Code) -and ($Code -le 0x10FFFF)) {
+	if ((0x10000 -le $Code) -and ($Code -le 0x10FFFF))
+	{
 		return [char]::ConvertFromUtf32($Code)
 	}
 
 	throw "Invalid character code $Code"
 }
 
-function Test-Administrator {
+function Test-Administrator
+{
 	$user = [Security.Principal.WindowsIdentity]::GetCurrent();
 	(New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
 
-function prompt {
-	function Initialize-Profile {
+function rprompt
+{
+	$pos = $Host.UI.RawUI.CursorPosition
+	$status = (Get-GitStatus -Force)
+	$b = $status.Branch
+	$l = $b.Length
+
+	if ($l -gt 0)
+	{
+		$l = $b.Length + 3
+
+		$text = (Write-Prompt " $(U 0xE725)" -ForegroundColor ([ConsoleColor]::Cyan))
+		$text += (Write-Prompt " $b" -ForegroundColor ([ConsoleColor]::Cyan))
+		if ($status.HasIndex)
+		{
+			$text += (Write-Prompt "$(U 0x25AA)" -ForegroundColor ([ConsoleColor]::Green))
+			$l += 1
+		}
+		if ($status.HasWorking)
+		{
+			if ($status.HasUntracked -and $status.Working.length -gt 1)
+			{
+				$text += (Write-Prompt "$(U 0x25AA)" -ForegroundColor ([ConsoleColor]::Red))
+				$l += 1
+			} elseif ($status.HasUntracked -eq $false)
+			{
+				$text += (Write-Prompt "$(U 0x25AA)" -ForegroundColor ([ConsoleColor]::Red))
+				$l += 1
+			}
+		}
+		if ($status.HasUntracked)
+		{
+			$text += (Write-Prompt "$(U 0x25AA)" -ForegroundColor ([ConsoleColor]::Blue))
+			$l += 1
+		}
+	}
+
+	if ($last = Get-History -Count 1)
+	{
+		$elapsed = ""
+		$delta = $last.EndExecutionTime.Subtract($last.StartExecutionTime).TotalSeconds
+		$timespan = (New-TimeSpan -Seconds $delta)
+		if ($timespan.Days -gt 0)
+		{
+			$elapsed = "{0}d" -f $timespan.Days
+		}
+		if ($timespan.Hours -gt 0)
+		{
+			$elapsed += "{0}h" -f $timespan.Hours
+		}
+		if ($timespan.Minutes -gt 0)
+		{
+			$elapsed += "{0}m" -f $timespan.Minutes
+		}
+		if ($elapsed -eq "")
+		{
+			$elapsed += "{0:N2}s" -f $timespan.Seconds
+		} else
+		{
+			if($timespan.Days -eq 0)
+			{
+				$elapsed += "{0}s" -f $timespan.Seconds 
+			}
+		}
+		$l += 1 + $elapsed.Length
+		$text += (Write-Prompt " $elapsed" -ForegroundColor "#808080")
+	}
+
+	$Host.UI.RawUI.CursorPosition = New-Object System.Management.Automation.Host.Coordinates ($Host.UI.RawUI.WindowSize.Width - $l),$Host.UI.RawUI.CursorPosition.Y
+
+	Write-Host $text -NoNewline
+	$Host.UI.RawUI.CursorPosition = $pos
+}
+
+function prompt
+{
+	function Initialize-Profile
+	{
 		Import-Module -Name Terminal-Icons
 		Import-Module PSReadLine
 
@@ -89,7 +170,7 @@ function prompt {
 		Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+r'
 		Set-PSReadLineKeyHandler -Key Ctrl+f -ScriptBlock {
 			$mru = @(
-				$env:USERPROFILE, 
+				$env:USERPROFILE,
 				$basedir,
 				-join($basedir, "Documents\_notes\zettelkasten"),
 				-join($basedir, "Git"),
@@ -99,16 +180,18 @@ function prompt {
 			$mru | Get-ChildItem -Attributes Directory | Invoke-Fzf | Set-Location
 			$previousOutputEncoding = [Console]::OutputEncoding
 			[Console]::OutputEncoding = [Text.Encoding]::UTF8
-			try {
+			try
+			{
 				[Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
-			}
-			finally {
+			} finally
+			{
 				[Console]::OutputEncoding = $previousOutputEncoding
 			}
 		}
 	}
 
-	if ($global:profile_initialized -ne $true) {
+	if ($global:profile_initialized -ne $true)
+	{
 		$global:profile_initialized = $true
 		Initialize-Profile
 	}
@@ -117,51 +200,37 @@ function prompt {
 
 	$history = Get-History -Count 1
 
-	if ($history) {
+	if ($history)
+	{
 		$command = $($history) -split " "
 		$host.ui.RawUI.WindowTitle = "$(Get-Location) | $($command[0])"
 	}
 
-	if (Test-Administrator) {
+	if (Test-Administrator)
+	{
 		# Highlight if elevated
 		Write-Host " $(U 0xE0A2) " -NoNewline -ForegroundColor Red -BackgroundColor Black
 	}
 
-	if ((Get-Location).providerpath -eq ($env:USERPROFILE)) {
+	if ((Get-Location).providerpath -eq ($env:USERPROFILE))
+	{
 		$cwd = "~"
-	}
-	else {
+	} else
+	{
 		$cwd = $(Get-Location | Split-Path -Leaf)
 	}
 	Write-Host $cwd -NoNewline -ForegroundColor DarkBlue -BackgroundColor Black
 
-	if ($status = Get-GitStatus -Force) {
-		Write-Host " $(U 0xE725)" -NoNewline -ForegroundColor Cyan 
-		Write-Host "$(Write-GitBranchName $status)" -NoNewline -ForegroundColor Cyan
-
-		if ($status.HasIndex) {
-			Write-Host "$(U 0x25AA)" -NoNewline -ForegroundColor Green 
-		}
-		if ($status.HasWorking) {
-			if ($status.HasUntracked -and $status.Working.length -gt 1) {
-				Write-Host "$(U 0x25AA)" -NoNewline -ForegroundColor Red 
-			}
-			elseif ($status.HasUntracked -eq $false) {
-				Write-Host "$(U 0x25AA)" -NoNewline -ForegroundColor Red 
-			}
-		}
-		if ($status.HasUntracked) {
-			Write-Host "$(U 0x25AA)" -NoNewline -ForegroundColor Blue 
-		}
-	}
-
-	if ($retval) {
+	if ($retval)
+	{
 		Write-Host " $(U 0x276F)" -NoNewline -ForegroundColor Green -BackgroundColor Black
-	}
-	else {
+	} else
+	{
 		Write-Host " $(U 0x276F)" -NoNewline -ForegroundColor Red -BackgroundColor Black
 	}
 	
+	rprompt
+
 	Remove-Variable retval
 
 	Write-Host "" -NoNewline -ForegroundColor White
