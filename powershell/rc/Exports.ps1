9f41fd7e68fa64923926f6b13ee9c75ea0a01260
Demaro Stanberry <demaro.stanberry@gmail.com>
2022-07-31 18:05:21 -0500

    modify fzf look and feel

--- powershell/rc/Exports.ps1
+++ powershell/rc/Exports.ps1
@@ -10,11 +10,16 @@ $env:FZF_DEFAULT_OPTS = '
 --scroll-off=3
 --bind=ctrl-d:preview-down
 --bind=ctrl-f:preview-up
+--bind=tab:toggle-out
+--bind=shift-tab:toggle-in
+--prompt=" "
+--pointer=""
+--marker=""
 --color=dark
 --color=fg:#bebebe,bg:-1,hl:#93b379
 --color=fg+:#dfe3ec,bg+:-1,hl+:#93b379
---color=info:#5f5f5f,prompt:#6f8fb4,pointer:#b04b57
---color=marker:#e5c179,spinner:#4c566a,header:#4c566a
+--color=info:#5f5f5f,prompt:#93b379,pointer:#bebebe
+--color=marker:#b04b57,spinner:#4c566a,header:#4c566a
 '
 
 $env:ZK_NOTEBOOK_DIR = $global:basedir + "Documents\_notes\zettelkasten\vault"
