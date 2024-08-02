7fdf31333ed09070255009c55c62df9aca6c624b
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-08-02 17:01:29 -0500

    chore: move `RPROMPT` content into `PROMPT`

--- powershell/Prompt.ps1
+++ powershell/Prompt.ps1
@@ -1,60 +1,47 @@
-function RPrompt {
+function get_vcs_metadata {
 	Import-Module posh-git
 	$global:GitPromptSettings.EnableStashStatus = $true
 	$status = (Get-GitStatus -Force)
 	$b = $status.Branch
-	$offset = $b.Length
-	$pos = $Host.UI.RawUI.CursorPosition
-
-	if ($offset -gt 0) {
+	if ($b.Length -gt 0) {
 		if ($null -ne $status.Upstream) {
 			$parts = $status.Upstream.Split("/")
 			if ($parts.Count -eq 2) {
 				$remote_name = $parts[1]
 				if ($b -ne $remote_name) {
 					$b = "$b→[$($status.Upstream)]"
-					$offset = $b.Length
 				}
 			}
 		}
 
-		$offset = $b.Length + 3
 		$text = (Write-Prompt " $(Get-Glyph 0xEA68)" -ForegroundColor ([ConsoleColor]::Cyan))
 		$text += (Write-Prompt " $b" -ForegroundColor ([ConsoleColor]::Cyan))
 		if ($status.AheadBy -gt 0) {
 			$text += (Write-Prompt "$(Get-Glyph 0x21E1)" -ForegroundColor ([ConsoleColor]::Red))
 			$text += (Write-Prompt "$($status.AheadBy)")
-			$offset += 2
 		}
 		if ($status.BehindBy -gt 0) {
 			$text += (Write-Prompt "$(Get-Glyph 0x21E3)" -ForegroundColor ([ConsoleColor]::Blue))
 			$text += (Write-Prompt "$($status.AheadBy)")
-			$offset += 2
 		}
 		if ($status.HasIndex) {
 			$text += (Write-Prompt "$(Get-Glyph 0x25AA)" -ForegroundColor ([ConsoleColor]::Green))
-			$offset += 1
 		}
 		if ($status.HasWorking) {
 			if ($status.HasUntracked -and $status.Working.length -gt 1) {
 				$text += (Write-Prompt "$(Get-Glyph 0x25AA)" -ForegroundColor ([ConsoleColor]::Red))
-				$offset += 1
 			}
 			elseif ($status.HasUntracked -eq $false) {
 				$text += (Write-Prompt "$(Get-Glyph 0x25AA)" -ForegroundColor ([ConsoleColor]::Red))
-				$offset += 1
 			}
 		}
 		if ($status.HasUntracked) {
 			$text += (Write-Prompt "$(Get-Glyph 0x25AA)" -ForegroundColor ([ConsoleColor]::Blue))
-			$offset += 1
 		}
 		if ($status.StashCount -gt 0) {
 			$text += (Write-Prompt " $(Get-Glyph 0xF02FB) " -ForegroundColor ([ConsoleColor]::Yellow))
-			$offset += 3
 		}
 	}
-
 	if ($last = Get-History -Count 1) {
 		$elapsed = ""
 		$delta = $last.EndExecutionTime.Subtract($last.StartExecutionTime).TotalMilliseconds
@@ -68,61 +55,46 @@ function RPrompt {
 		if ($timespan.Minutes -gt 0) {
 			$elapsed += "{0}m" -f $timespan.Minutes
 		}
-		if ($elapsed -eq "") {
-			$seconds = [int]$timespan.Seconds + ([int]$timespan.Milliseconds / 1000)
-			$elapsed += "{0:N2}s" -f $seconds
+		if ($elapsed -eq "" && $timespan.Seconds -gt 0) {
+			$elapsed += "{0}s" -f $timespan.Seconds
 		}
-		else {
-			if ($timespan.Days -eq 0) {
-				$elapsed += "{0}s" -f $timespan.Seconds 
-			}
+		if ($elapsed -ne "0s") {
+			$text += (Write-Prompt " $elapsed" -ForegroundColor "#808080")
 		}
-		$offset += 2 + $elapsed.Length
-		$text += (Write-Prompt " $elapsed" -ForegroundColor "#808080")
 	}
-
-	$Host.UI.RawUI.CursorPosition = New-Object `
-		System.Management.Automation.Host.Coordinates ($Host.UI.RawUI.WindowSize.Width - $offset), $Host.UI.RawUI.CursorPosition.Y
 	Write-Host $text -NoNewline
-	$Host.UI.RawUI.CursorPosition = $pos
 }
 
 function global:Prompt {
 	$origRetval = $global:?
-
 	$current_working_directory = $(Get-Location | Split-Path -Leaf)
 	$command = ""
-
 	$history = $(Get-History -Count 1 | Select-Object -ExpandProperty CommandLine)
-
 	if ($history) { $command = $history	}
-
 	if (Test-Administrator) {
 		Write-Host "$(Get-Glyph 0xF0483) " -NoNewline -ForegroundColor Red -BackgroundColor Black
 	}
-
 	if (!$null -eq $env:VIRTUAL_ENV) {
 		Write-Host "($($env:VIRTUAL_ENV | Split-Path -Leaf)) " -NoNewline -ForegroundColor Yellow -BackgroundColor Black
 	}
-
 	if ((Get-Location).providerpath -eq ($env:USERPROFILE)) {
 		$cwd = "~"
 	}
 	else {
 		$cwd = $current_working_directory
 	}
-
 	if ($command -eq "") {
 		$Host.ui.RawUI.WindowTitle = "$current_working_directory"
 	}
 	else {
 		$Host.ui.RawUI.WindowTitle = "$current_working_directory | $command"
 	}
-
 	Write-Host "$([char]27)[1m$cwd$([char]27)[24m" -NoNewline -ForegroundColor DarkBlue -BackgroundColor Black
 
+	get_vcs_metadata
+
 	if ($origRetval) {
-		Write-Host " $([char]27)[1m$(Get-Glyph 0x276F)$([char]27)[24m" -NoNewline -ForegroundColor Green -BackgroundColor Black
+		Write-Host "`n $([char]27)[1m$(Get-Glyph 0x276F)$([char]27)[24m" -NoNewline -ForegroundColor Green -BackgroundColor Black
 	}
 	else {
 		$historyPath = (Get-PSReadLineOption).HistorySavePath
@@ -133,24 +105,17 @@ function global:Prompt {
 				Set-Content -Path $historyPath -Value ($historyContent | Select-String -Pattern $global:LASTHIST -NotMatch)
 			}
 		}
-		Write-Host " $([char]27)[1m$(Get-Glyph 0x276F)$([char]27)[24m" -NoNewline -ForegroundColor Red -BackgroundColor Black
+		Write-Host "`n $([char]27)[1m$(Get-Glyph 0x276F)$([char]27)[24m" -NoNewline -ForegroundColor Red -BackgroundColor Black
 	}
-
 	Remove-Variable origRetval
 	Remove-Variable LASTHIST -Scope "Global"
-
-	RPrompt
-
 	$loc = $executionContext.SessionState.Path.CurrentLocation;
 	$current_context = "$([char]27)]9;12$([char]7)"
-
 	if ($loc.Provider.Name -eq "FileSystem") {
 		$current_context += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
 	}
-
 	Write-Host $current_context -NoNewline 
 	Write-Host "" -NoNewline -ForegroundColor White
 	Write-Host -NoNewLine "`e[2 q"
-
 	return " "
 }
